5c781c7cb5a8dbac60ebaafc776970dd6ed8e29d
Christian Wach <needle@haystack.co.uk>
2015-07-15 12:43:33 +0100

    first pass at reusing templates

---
+++ templates/profile-header-drafts-social-paper.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Header template displayed on draft social paper listings on member profiles.
+ *
+ * @package Social_Paper
+ * @subpackage Template
+ */
+?>
+<p><?php _e( 'Your draft papers.', 'social-paper' ); ?></p>
---
+++ templates/profile-header-publish-social-paper.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Header template displayed on published social paper listings on member profiles.
+ *
+ * @package Social_Paper
+ * @subpackage Template
+ */
+?>
+<p><?php _e( 'Your published papers.', 'social-paper' ); ?></p>
