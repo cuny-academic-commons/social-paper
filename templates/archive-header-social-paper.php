e21809bc94f2200ec72f48083b23a4d7cb9ee694
Christian Wach <needle@haystack.co.uk>
2015-07-15 12:37:58 +0100

    separate out template parts

---
+++ templates/archive-header-social-paper.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Header template displayed on social paper archive.
+ *
+ * @package Social_Paper
+ * @subpackage Template
+ */
+?>
+<p><?php _e( 'Here you can view a list of papers written by members of the community.', 'social-paper' ); ?></p>
---
+++ templates/archive-item-social-paper.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Item template displayed on social paper archive.
+ *
+ * @package Social_Paper
+ * @subpackage Template
+ */
+?>
+<li><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></li>
