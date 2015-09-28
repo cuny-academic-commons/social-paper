d1c9be3d971bc7a819b43a2e4d044bba6f55cecd
r-a-y <r-a-y@gmx.com>
2015-09-27 19:39:31 -0700

    Capabilities: Ensure that subscribers and contributors can create papers.
    Also includes unit test.
    
    See #16.

--- includes/schema.php
+++ includes/schema.php
@@ -34,6 +34,8 @@ function cacsp_register_post_types() {
 	register_post_type( 'cacsp_paper', array(
 		'labels' => $labels,
 		'public' => true,
+		'capability_type' => 'paper',
+		'map_meta_cap' => true,
 		'rewrite' => array(
 			'slug' => 'papers',
 			'with_front' => false,
@@ -58,3 +60,68 @@ function cacsp_register_taxonomies() {
 		),
 	) );
 }
+
+/**
+ * Add capabilities for subscribers and contributors.
+ *
+ * By default, subscribers and contributors do not have caps to post, edit or
+ * delete papers. This function injects these caps for users with these roles.
+ *
+ * @param  array  $caps    The mapped caps
+ * @param  string $cap     The cap being mapped
+ * @param  int    $user_id The user id in question
+ * @param  array  $args    Optional parameters passed to has_cap(). For us, this means the post ID.
+ * @return array
+ */
+function cacsp_map_basic_meta_caps( $caps, $cap, $user_id, $args ) {
+	switch ( $cap ) {
+		// give user these caps
+		case 'publish_papers' :
+		case 'edit_papers' : // handles adding tags/categories
+			break;
+
+		// meta caps
+		case 'edit_paper' :
+		case 'delete_paper' :
+			// bail if someone else's event
+			if ( false !== strpos( $caps[0], 'others' ) ) {
+				return $caps;
+			}
+
+			break;
+
+		case 'read_paper' :
+			if ( get_post( $args[0] )->post_status === 'publish' ) {
+				return array( 'exist' );
+			}
+
+			// Make sure authors can view their own post
+			if ( (int) get_post( $args[0] )->post_author === $user_id ) {
+				return array( 'exist' );
+			}
+
+			return $caps;
+			break;
+
+		default :
+			return $caps;
+			break;
+	}
+
+	// make sure user is valid
+	$user = new WP_User( $user_id );
+	if ( ! is_a( $user, 'WP_User' ) || empty( $user->ID ) ) {
+		return $caps;
+	}
+
+	/**
+	 * Filters Social Paper basic meta caps.
+	 *
+	 * @param array   Pass 'exist' cap so users are able to manage papers.
+	 * @param array   $caps The mapped caps
+	 * @param string  $cap The cap being mapped
+	 * @param WP_User The user being tested for the cap.
+	 */
+	return apply_filters( 'cacsp_map_basic_meta_caps', array( 'exist' ), $caps, $cap, $user );
+}
+add_filter( 'map_meta_cap', 'cacsp_map_basic_meta_caps', 15, 4 );
---
+++ tests/phpunit/tests/caps.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @group caps
+ */
+class CACSP_Tests_Caps extends CACSP_UnitTestCase {
+	protected $current_user;
+	protected $user;
+
+	public function setUp() {
+		parent::setUp();
+		$this->current_user = bp_loggedin_user_id();
+	}
+
+	public function tearDown() {
+		$this->set_current_user( $this->current_user );
+	}
+
+	public function test_subscriber_can_publish_paper() {
+		$u = $this->factory->user->create();
+		$this->set_current_user( $u );
+		$this->assertTrue( current_user_can( 'publish_papers' ) );
+	}
+}
