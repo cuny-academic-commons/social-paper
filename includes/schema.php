cuny-academic-commons/social-paper

commit 8842a6a54d4be8d6452f5a13cf3f9608ec74b6cc

Capabilities: Allow subscribers to upload files for social papers.
See #5.

includes/schema.php
@@ -179,6 +179,25 @@ function cacsp_map_extra_meta_caps( $caps, $cap, $user_id, $args ) {
 			return $caps;
 			break;
 
+		// allow files to be uploaded via AJAX
+		case 'upload_files' :
+			if ( defined( 'DOING_AJAX' ) || true === constant( 'DOING_AJAX' ) ) {
+				if ( false === isset( $_REQUEST['post_id'] ) ) {
+					return $caps;
+				}
+
+				if ( false === current_user_can( 'edit_paper', $_REQUEST['post_id'] ) ) {
+					return $caps;
+				}
+
+				$user_id = get_current_user_id();
+
+			} else {
+				return $caps;
+			}
+
+			break;
+
 		default :
 			return $caps;
 			break;

commit 0d6af4394333b6e0723dbd81799b4bd5712ed1db

Correct notice caused by typo in 8842a6a54d4be8d6452f5a13cf3f9608ec74b6cc.
See #5.

includes/schema.php
@@ -181,7 +181,7 @@ function cacsp_map_extra_meta_caps( $caps, $cap, $user_id, $args ) {
 
 		// allow files to be uploaded via AJAX
 		case 'upload_files' :
-			if ( defined( 'DOING_AJAX' ) || true === constant( 'DOING_AJAX' ) ) {
+			if ( defined( 'DOING_AJAX' ) && true === constant( 'DOING_AJAX' ) ) {
 				if ( false === isset( $_REQUEST['post_id'] ) ) {
 					return $caps;
 				}