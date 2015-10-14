e91bef1a1e6b837b71e781d5e74dcedc9e883497
Christian Wach <needle@haystack.co.uk>
2015-10-14 12:12:40 +0100

    prevent JS error for logged-out users

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,12 +1,14 @@
 ( function( $ ){
 	$( document ).ready( function(){
-		$( '#cacsp-group-selector' ).select2( {
-			placeholder: SocialPaperL18n.group_placeholder
-		} );
+		if ( 'undefined' !== typeof CACSP_Potential_Readers ) {
+			$( '#cacsp-group-selector' ).select2( {
+				placeholder: SocialPaperL18n.group_placeholder
+			} );
 
-		$( '#cacsp-reader-selector' ).select2( {
-			placeholder: SocialPaperL18n.reader_placeholder,
-			data: CACSP_Potential_Readers
-		} );
+			$( '#cacsp-reader-selector' ).select2( {
+				placeholder: SocialPaperL18n.reader_placeholder,
+				data: CACSP_Potential_Readers
+			} );
+		}
 	} );
 }( jQuery ) );
