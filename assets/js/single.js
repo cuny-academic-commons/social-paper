59aad163f4d6d489fba8617da39f8448541e151c
Daniel Jones <drjones18@gmail.com>
2016-05-11 18:25:36 -0400

    Fix typo in last commit: check to make sure that group_id string is not empty, not that the i18n string is not empty.

--- assets/js/single.js
+++ assets/js/single.js
@@ -39,7 +39,7 @@
 			var cacsp_group_selector = $( '#cacsp-group-selector' ).select2( {
 				placeholder: SocialPaperI18n.group_placeholder
 			} );
-			if ( SocialPaperI18n !== '' ) {
+			if ( SocialPaperI18n.group_id !== '' ) {
 				cacsp_group_selector.select2( 'val', SocialPaperI18n.group_id );
 			}
 		}
